d7c0a9e2aa79e0e648bdce12afa73c78acd0d963
Galang, Stephanie Capule <sgalang3@gatech.edu>
2018-11-16 00:02:17 -0500

    Update reddit.py
    Use Reddit API to pull data, instantiate Kinesis client with boto3

--- reddit.py
+++ reddit.py
@@ -1,5 +1,12 @@
 import praw
+import boto3
+from boto import kinesis
 
+AWS_KEY="AKIAIDYW3QB6YVXKURVQ"
+AWS_SECRET="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"
+REGION="us-east-2"
+
+# ------ Get newest postings from Reddit ------ #
 reddit = praw.Reddit(client_id='iW3F2X0p_3e9ag',
                      client_secret='ciLt6KkvKawJYzLG5wWQTzADMq0',
                      user_agent='Cloud Computing Project (catappstrophe) by sgalang3')
@@ -11,3 +18,19 @@ for submission in reddit.subreddit('aww').new(limit=10):
     #print(submission.permalink) link to the page
     #print(submission.created_utc) time created
     print(submission.url) # if subdomain is https://i.redd.it/, then is a pullable image
+
+# ------ Get newest postings from Reddit ------ #
+#Connect to Kinesis Stream
+client = boto3.client('kinesis', aws_access_key_id=AWS_KEY,
+                            aws_secret_access_key=AWS_SECRET,
+                            region_name=REGION)
+
+
+print client.list_streams()
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html#Kinesis.Client.put_record
+# response = client.put_record(
+#     StreamName='Catappstrophe',
+#     Data=b'bytes',
+#     PartitionKey='string'
+# )
