7378aa7127cd67d62693d3436ff0d53844d2f71e
Galang, Stephanie Capule <sgalang3@gatech.edu>
2018-11-16 03:07:56 -0500

    Kinesis loops
    Places any new posts into stream (continually loops)

--- reddit.py
+++ reddit.py
@@ -2,7 +2,7 @@ import praw
 import boto3
 import json
 from boto import kinesis
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # ------ Set Up Credential File ------ #
 # pip install configparser
@@ -14,42 +14,45 @@ AWS_KEY = str(config.get('auth', 'AWS_KEY'))
 AWS_SECRET = config.get('auth', 'AWS_SECRET').encode('utf-8')
 REGION = config.get('auth', 'REGION').encode('utf-8')
 
-# ------ Get newest postings from Reddit ------ #
+# ------ Connect to Reddit API ------ #
 reddit = praw.Reddit(client_id='iW3F2X0p_3e9ag',
                      client_secret='ciLt6KkvKawJYzLG5wWQTzADMq0',
                      user_agent='Cloud Computing Project (catappstrophe) by sgalang3')
 
-for submission in reddit.subreddit('aww').new(limit=10):
-	# https://praw.readthedocs.io/en/latest/code_overview/models/submission.html?highlight=.url
-    link = submission.url # url of the image
-
-    # ------ Logic to get only images ------ #
-    # if subdomain is https://i.redd.it/, then is a pullable image
-    if link[:18] == "https://i.redd.it/" or link[:18] == "https://imgur.com/":
-	    passData = {
-	    	"title": submission.title.encode('utf-8'),
-	    	"img-url": submission.url, # url of the image
-	    	"timestamp": datetime.utcfromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S'), # time created in utc
-	    	"permalink": submission.permalink # path to the page, str
-	    }
-	    print (passData)
-	    
-	    break
-
-# ------ Kinesis stream code ------ #
-#Connect to Kinesis Stream
+# ------ Connect to Kinesis Stream ------ #
 client = boto3.client('kinesis', aws_access_key_id=AWS_KEY,
                             aws_secret_access_key=AWS_SECRET,
                             region_name=REGION)
 
-# print client.list_streams() # read what streams exist in client
+if __name__ == '__main__':
+    print ('Listening...')
+    # ------ Get newest image postings from Reddit ------ #
+    subreddit = reddit.subreddit('aww')
+    for submission in subreddit.stream.submissions():
+        if (datetime.utcnow() - timedelta(minutes=0) <= datetime.utcfromtimestamp(submission.created_utc)): # Only analyze images from past 0 mins, else stream will provide previous hours' posts
+            print ('----------------------------------------------------------------------')
+            # https://praw.readthedocs.io/en/latest/code_overview/models/submission.html?highlight=.url
+            link = submission.url # url of the image
 
-# publish a record
-# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html#Kinesis.Client.put_record
-response = client.put_record(
-    StreamName='Catappstrophe',
-    Data=json.dumps(passData),
-    PartitionKey='partitionKey1'
-)
+            # ------ Logic to get only images ------ #
+            # if domain is https://i.redd.it/ or https://imgur.com/, then is a pullable image
+            if link[:18] == "https://i.redd.it/" or link[:18] == "https://imgur.com/":
+                passData = {
+                    "title": submission.title.encode('utf-8'),
+                    "img-url": submission.url, # url of the image
+                    "timestamp": datetime.utcfromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S'), # time created in utc
+                    "permalink": submission.permalink # path to the page, str
+                }
+                print (passData)
+                
+                # ------ Publish a record to Kinesis ------ #
+                # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html#Kinesis.Client.put_record
+                # response = client.put_record(
+                #     StreamName='Catappstrophe',
+                #     Data=json.dumps(passData),
+                #     PartitionKey='partitionKey1'
+                # )
 
-print (response)
+                # print (response)
+            else:
+                print ("Post is not an image!")
