3ee55d61fc3793a422d954b7131ac0a9e00ccee4
rachuang22 <rachuang22@gmail.com>
2018-11-17 15:51:12 -0500

    new lambda1 function

---
+++ lambda_functions/Kinesis_S3.py
@@ -0,0 +1,18 @@
+import boto3
+from PIL import Image
+import urllib.request
+import io
+
+# pass on url.jpg + image itself to s3
+def my_handler(event, context):
+    return event['img-url']
+
+getting the image from the url
+with urllib.request.urlopen(URL) as url:
+    f = io.BytesIO(url.read())
+
+img = Image.open(f)
+
+s3.put_object(Bucket=BUCKET,
+              Key=url,
+              Body=img)
